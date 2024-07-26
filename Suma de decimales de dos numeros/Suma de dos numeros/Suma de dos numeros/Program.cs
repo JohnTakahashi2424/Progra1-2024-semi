3d0110675f75d78716948dadb3b14dfd0b72edde
Jonathan Caleb Guandique Flores <Takahashijohnxd01@gmail.com>
2024-07-25 20:29:16 -0600

    SUMA DE DOS NUMEROS

---
+++ Suma de decimales de dos numeros/Suma de dos numeros/Suma de dos numeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_de_dos_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //suma de decimales de dos numeros
+
+            Console.Write("num 1:");
+            double num1 = double.Parse(Console.ReadLine()); //"5" ->
+
+            Console.Write("num 2:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Double respuesta = num1 + num2;
+            Console.WriteLine("la suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}
