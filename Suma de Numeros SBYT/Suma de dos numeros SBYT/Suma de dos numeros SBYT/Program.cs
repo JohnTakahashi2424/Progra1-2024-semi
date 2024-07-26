ec63b373c2d07095718116d6ccd394106edec71a
Jonathan Caleb Guandique Flores <Takahashijohnxd01@gmail.com>
2024-07-25 21:19:58 -0600

    Suma de numeros SBYT

---
+++ Suma de Numeros SBYT/Suma de dos numeros SBYT/Suma de dos numeros SBYT/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_de_dos_numeros_SBYT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Suma de dos numeros SBYT
+
+            Console.Write("num 1:");
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
+
+            Console.Write("num 2:");
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+
+            Console.WriteLine("la suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}
