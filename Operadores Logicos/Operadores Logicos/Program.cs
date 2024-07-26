1293197027f3665cb2a07a01a91a215c2a021b9f
Jonathan Caleb Guandique Flores <Takahashijohnxd01@gmail.com>
2024-07-25 20:51:42 -0600

    Operadores Logicos

---
+++ Operadores Logicos/Operadores Logicos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_Logicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operadores logicos ejemplo
+
+            int respuesta = 5 + 8 - 2 * 5 / 4 + 1; //12.00
+            Console.WriteLine("la respuesta es: {0})", respuesta);
+
+            Console.WriteLine("la divison de 5/4 es {0}", 5 / 4);
+            //Pausa
+            Console.ReadLine();
+        }
+    }
+}
