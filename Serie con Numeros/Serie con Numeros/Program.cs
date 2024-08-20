d366020288e0a5fe0abef21003a4ae108b5b6ca7
Jonathan Caleb Guandique Flores <Takahashijohnxd01@gmail.com>
2024-08-20 07:34:06 -0600

    Serie con Numeros

---
+++ Serie con Numeros/Serie con Numeros/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_con_Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Determinar si cada numero de la serie es par o impar
+
+            int[] serie = new int[] { 5, 9, 4, 6, 3, 2 };
+            foreach (int num in serie)
+            {
+                Console.WriteLine("El num {0} es {1}", num, num % 2 == 0 ? "par" : "impar");
+            }
+            Console.ReadLine();
+        }
+    }
+}
